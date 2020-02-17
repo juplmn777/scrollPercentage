e28471a8bc70366197409393de847ecb50dbd053
ju <plumyoen.julien@gmail.com>
2020-02-17 16:08:05 +0100

    event scroll défini & ok

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const bar = document.querySelector(".bar");
 
 addEventListener("scroll", function(){
-    console.log(`
-        taille de la page: ${document.body.scrollHeight}
-        hauteur a afficher: ${innerHeight}
-        position du scroll: ${pageYOffset}
-    `);
+const max = document.body.scrollHeight - innerHeight;
+const percentage = pageYOffset / max * 100;
+
+bar.style.width  = percentage + "%";
+
+    // console.log(`
+    //     taille de la page: ${document.body.scrollHeight}
+    //     hauteur a afficher: ${innerHeight}
+    //     position du scroll: ${pageYOffset}
+    // `);
 });
