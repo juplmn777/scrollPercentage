c38f290afc54846c679701d0a54bb6967adad6a9
ju <plumyoen.julien@gmail.com>
2020-02-17 14:59:52 +0100

    élément à récupérer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const bar = document.querySelector(".bar");
 
 addEventListener("scroll", function(){
-    
-})
+    console.log(`
+        taille de la page: ${document.body.scrollHeight}
+        hauteur a afficher: ${innerHeight}
+        position du scroll: ${pageYOffset}
+    `);
+});
